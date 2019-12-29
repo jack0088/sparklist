dfcd1557df60aabf9a96241a5f292f10db429682
herrsch <jack0088@me.com>
2019-12-30 00:04:00 +0100

    removed test code

--- server.lua
+++ server.lua
@@ -1,21 +1,5 @@
 -- 2019 (c) kontakt@herrsch.de
 
-
-test = {}
-test.foobar = require "foobar"
--- test = require "foobar"
-
-local hotswap = require "hotswap"
-
-while true do
-    hotswap:run()
-    if not ko or ko < os.time() then
-        ko = os.time() + .5
-        test.foobar:speak()
-    end
-end
-
-
 package.path = "./?/init.lua;"..package.path -- Lua <= 5.1
 
 local hotswap = require "hotswap"

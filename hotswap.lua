d14cfce236577c19b93051ec003bda50648e1ccd
herrsch <jack0088@me.com>
2019-12-30 20:44:17 +0100

    added safty checking for error messages printing

--- hotswap.lua
+++ hotswap.lua
@@ -129,12 +129,14 @@ function require(resource, force_reload) -- override standard Lua function!
         ))
         return message
     end
-    print(debug.traceback(string.format(
-        "%s failed to hot-reload module '%s'\nerror message: %s",
-        os.date("%d.%m.%Y %H:%M:%S"),
-        resource,
-        message
-    )))
+    if not success and message ~= nil then
+        print(debug.traceback(string.format(
+            "%s failed to hot-reload module '%s'\nerror message: %s",
+            os.date("%d.%m.%Y %H:%M:%S"),
+            resource,
+            message or "???"
+        )))
+    end
 end
 
 

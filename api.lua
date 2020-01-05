26824a747d3fa712eebe6c4e4dc8a16a3aa5feb9
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-01-05 16:59:40 +0100

    testing

--- api.lua
+++ api.lua
@@ -27,7 +27,9 @@ api:get("/chunked%-message", function(request, response)
     print "route fired..."
     request:receiveMessage(function(chunk)
         print("chunk received:", chunk)
+        coroutine.yield(chunk)
     end)
+    -- read another file and chunk respond with its contents here...
     print("request.message:", request.message)
     return response:submit("foobar")
 end)

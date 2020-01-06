5857b3c01375b4481ee02d8a7ec5670ab3f1556a
herrsch <jack0088@me.com>
2020-01-06 15:47:26 +0100

    updated reference to the renamed view

--- controllers/index.lua
+++ controllers/index.lua
@@ -1,5 +1,5 @@
 local view = require "views"
 
 return function(request, response)
-    return response:submit(view("landing"), "text/html")
+    return response:submit(view("index"), "text/html")
 end

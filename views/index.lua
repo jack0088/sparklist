cb9dffcb548916b1b70afd972afa5a12dfd838e0
herrsch <jack0088@me.com>
2020-01-06 15:46:21 +0100

    renamed landing to index (like index.html would be named)

---
+++ views/index.lua
@@ -0,0 +1,24 @@
+local dom = require "dom"
+
+local page = dom{
+    dom["!doctype"] "html",
+    dom.html{
+        dom.header{
+            dom.title "sparklist.io",
+            dom.meta{charset = "utf-8"},
+            dom.style{type = "text/css", innerHTML = css}
+        },
+        dom.body{
+            dom.button{innerHTML = "Login with SwoopJS", id="swooplogin"},
+            dom.script{src = "https://app.swoopnow.com/swoop.js"},
+            dom.script "Swoop.init('wb_VVknxPn8YkxMipKjFWEehw'); document.getElementById('swooplogin').addEventListener('click', function(){Swoop.open('login');});",
+            dom.p "show some user pictures here",
+            dom.p "some other content over here",
+            dom.footer "<i>maybe a copyright 2020-2021 here and other stuff down here</i>"
+        }
+    }
+}
+
+return function()
+    return page.htmlsource
+end

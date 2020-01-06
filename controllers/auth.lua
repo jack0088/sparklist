7a450cb43917ca64fc1697caf88aa70841625d00
herrsch <jack0088@me.com>
2020-01-06 15:44:30 +0100

    renamed login to auth and need to rework this (seems like when you have token then you always logged in? or need to always veryfy this againts the API?)

---
+++ controllers/auth.lua
@@ -0,0 +1,50 @@
+-- 2019 (c) kontakt@herrsch.de
+
+-- SwoopJS is a paswordless single-sign-on service
+-- see docs for more information https://swoopnow.com/documentation
+
+-- NOTE the service depends on a tls connection when working with their API
+-- NOTE swoop uses DKIM and SPF to verify the identity of the sender it also depends on SNI (server name indication) which is not supported by every ssl-library!
+-- plain socket.http.request() will fail and always responds with a status code 301
+
+-- NOTE on some linux distribustion you need to run `sudo apt-get install libssl-dev`
+-- to get the header files as well as all default install directories
+
+-- possible dependencies are openssl (or luasec or lua-http, which both bundle openssl); luasec is recomended
+-- NOTE when having trouble installing luasec, see https://github.com/luarocks/luarocks/issues/579
+
+-- I decided to keep it simple and use plain HTTP everywhere
+-- for security reasons however I'll should hide the xors server behind a reverse SSL proxy like cloudflare
+
+
+local https = require "ssl.https"
+
+return function(request, response, token)
+    -- client uses swoops fontend plugin that opens a form and sends a login request
+    -- swoopjs receives that login request and generates a resonse containing a user identification token
+    -- that swoopjs response is forwarded onto an url specified in their admin panel
+    -- once swoopjs has requested this route with the token we need to check the credentials
+    -- the server (xors) requests the swoopjs API with the token https://app.swoopnow.com/api/inbound_emails/d98fc8d31ceb2bf9208dae120530e2bf
+    -- if the user is- or has been logged-in successfully then out respose is a plain/text containing the email of the user and a status code of 200, otherwise status code of 404 and no message
+
+    print "auth route..."
+
+    local cookie = request.header["cookie: swoopid"] -- TODO!!!!
+    local identity = token or cookie
+    print(identity, token, cookie)
+
+    if identity then
+        local email, status = https.request("https://app.swoopnow.com/api/inbound_emails/"..identity) -- TODO add timeout of 1s
+        print(string.format("%s swoop login response with token=%s, state=%s, email=%s"
+            os.date("%d.%m.%Y %H:%M:%S"),
+            identity,
+            status,
+            email
+        )
+        if status == 200 and email then
+            response:addHeader("Set-Cookie", "swoopid="..identity) -- create new or update existing
+            return response:submit(view("profile", email), "text/html")
+        end
+    end
+    return response:redirect("/", nil, 200)
+end

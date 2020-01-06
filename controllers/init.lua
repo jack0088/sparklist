3e51c49bd2b4c570267a97385f4dba1f0b477338
herrsch <jack0088@me.com>
2020-01-06 15:45:55 +0100

    added copyright comments

--- controllers/init.lua
+++ controllers/init.lua
@@ -1,7 +1,10 @@
+-- 2019 (c) kontakt@herrsch.de
+
 local function controller(handler)
     if not handler:match("^controllers.+") then handler = "controllers."..handler end
     local status, delegate = pcall(dofile, handler:gsub("%.", "/")..".lua")
     if status and type(delegate) == "function" then return delegate end
     return controller "404"
 end
+
 return controller
--- views/init.lua
+++ views/init.lua
@@ -1,7 +1,10 @@
+-- 2019 (c) kontakt@herrsch.de
+
 local function view(template, ...)
     if not template:match("^views.+") then template = "views."..template end
     local status, delegate = pcall(dofile, template:gsub("%.", "/")..".lua")
     if status and type(delegate) == "function" then return delegate(...) end
     return view "404"
 end
+
 return view

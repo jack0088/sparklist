e94ad792ccd51d724f9923dd43f1cd0d343908ae
herrsch <jack0088@me.com>
2020-01-06 15:45:12 +0100

    test with chunked responses worked i guess chunked requests work as well (haven found a way to test) - added some docs to it for later use

--- controllers/chunked_messages.lua
+++ controllers/chunked_messages.lua
@@ -1,3 +1,15 @@
+-- 2019 (c) kontakt@herrsch.de
+
+-- fallowing is an example how chunked requests and responses are handled correctly
+-- this controller function is actually always wrapped into a coroutine, later inside the dispatcher (who's an instance of the router, so look inside the router to find the wrapping routine)
+-- at any point, you can yield this function and let another client be served until continuing this operation on the next frame
+
+-- responses are blocking loops (chunked requests or not) which you can yield anyways by supplying a steram_sink function to the :receiveMessage() method of the response
+-- inside that sink you can now process each chunk of the stream (or the whole request message if no Transfer-Encoding: chunkt were used)
+
+-- similar applies to responses - they blocking as well, but when writing only chunk by chunk you can use a while loop for example
+-- now, that loop can be yielded as well as you can guess :)
+
 return function(request, response)
     print "receiving chunked request (if any):"
     request:receiveMessage(function(chunk)

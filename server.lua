49e277a7f7654ada493d37fa1f2e02b3378c4bea
herrsch_de <jack0088@me.com>
2020-01-16 15:59:19 +0100

    changed variable names

--- server.lua
+++ server.lua
@@ -3,14 +3,14 @@
 package.path = "./?/init.lua;"..package.path -- Lua <= 5.1
 
 require "log"
-local hotswap = require "hotswap"
 
+local hotswap = require "hotswap"
 local server = require "xors"{
     port = 80,
     timeout = 0.1,
     backlog = 100,
 	plugins = {
-        hotswap,
-        aquire "dispatcher"
+        hotswap, -- upddates via :onEnterFrame
+        hotswap "dispatcher"
     }
 }:run()

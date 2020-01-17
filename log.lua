2a75d80e00a8c77f61018b5941f0795849166018
herrsch_de <jack0088@me.com>
2020-01-17 07:44:59 +0100

    renamed vars and added var for path to easiert see and change if needed

--- log.lua
+++ log.lua
@@ -2,12 +2,13 @@
 
 -- NOTE every day a new log file is created and filled with messages from the command line console
 
-local log = dofile "utilities.lua".writefile
+local write = dofile "utilities.lua".writefile
+local path = "logs/"
 local _print = print
 
 function print(...)
     local dump = ""
     for _, argument in ipairs{...} do dump = dump..tostring(argument).."    " end
-    log("logs/"..os.date("%Y-%m-%d")..".txt", dump:sub(1, -4).."\n", "a")
+    write(path..os.date("%Y-%m-%d")..".txt", dump:sub(1, -4).."\n", "a")
     _print(...)
 end

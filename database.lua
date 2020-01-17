a5191175400c0eb3c9d6c9db9db8d2bcbb6a92ec
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-01-17 16:54:05 +0100

    improved how the wrapper func can be used for placeholder sql_query and variables, that fill out the placeholders (to avoid using string.format too often when writing complicated queries)

--- database.lua
+++ database.lua
@@ -5,6 +5,7 @@
 -- LuaSQL documentation at https://keplerproject.github.io/luasql/manual.html
 
 
+local unpack = unpack or table.unpack -- Lua > 5.1
 local sql = require "luasql.sqlite3".sqlite3()
 local class = require "class"
 local Database = class()
@@ -31,9 +32,27 @@ function Database:disconnect()
 end
 
 
-function Database:run(sql_statement, request_sink)
+-- possible parameters are (in order)
+-- @sql_query (required string) the sql statement you want to execute on the database
+-- @... (optional any) when @sql_query uses a string with placeholders inside, e.g. %s like in string.format, then @... can be any type or amount of optional parameters that will be forwarded to string.format("", ...) to fill out the placeholders
+-- @request_sink (optional function) handler that can process each row of the returned database records; it works pretty much the same as Requst:receiveMessage(stream_sink); the idea is to allow Database.run to be executed in a threaded manner
+function Database:run(sql_query, ...)
     self:connect()
-    local cursor = assert(self.connection:execute(sql_statement:gsub("%s+", " "))) -- single transaction (auto-commit mode)
+    local sql_statement = sql_query:gsub("%s+", " ")
+    local variables = {...}
+    local request_sink = variables[#variables]
+
+    if type(request_sink) == "function" then
+        table.remove(variables, #variables)
+    else
+        request_sink = nil
+    end
+
+    if #variables > 0 then
+        sql_statement = string.format(sql_statement, unpack(variables))
+    end
+
+    local cursor = assert(self.connection:execute(sql_statement)) -- single transaction (auto-commit mode)
     local dataset, row
     if type(cursor) == "userdata" then
         repeat

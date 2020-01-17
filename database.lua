ca9a72a4d1f1665793efce51690f8819d4c9a2d0
herrsch_de <jack0088@me.com>
2020-01-17 12:17:00 +0100

    added database connector abstraction layer to work through with db

---
+++ database.lua
@@ -0,0 +1,52 @@
+-- 2020 (c) kontakt@herrsch.de
+
+local sql = require "luasql.sqlite3".sqlite3()
+local class = require "class"
+local Database = class()
+
+
+function Database:new(file)
+    self.timeout = 1
+    self.file = file
+end
+
+
+function Database:connect()
+    self:disconnect()
+    assert(type(self.file) == "string", "missing database file path")
+    self.connection = sql:connect(self.file, self.timeout or 1)
+end
+
+
+function Database:disconnect()
+    if self.connection then
+        self.connection:close()
+        self.connection = nil
+    end
+end
+
+
+function Database:run(sql_statement, request_sink)
+    self:connect()
+    local cursor = assert(self.connection:execute(sql_statement)) -- single transaction (auto-commit mode)
+    local dataset, row
+    repeat
+        row = cursor:fetch({}, "a")
+        if row then
+            if type(request_sink) == "function" then
+                -- NOTE @request_sink works similar to Requst:receiveMessage(stream_sink)
+                -- and is advised to be used together with coroutine.yield for large requests to the database
+                request_sink(row)
+            else
+                if not dataset then dataset = {} end
+                table.insert(dataset, row)
+            end
+        end
+    until not row
+    cursor:close()
+    self:disconnect()
+    return dataset
+end
+
+
+return Database

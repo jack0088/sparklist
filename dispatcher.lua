80aa819f2b6599fdff48f8251d40c90292f8e1e8
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-01-18 18:54:03 +0100

    fixed little bug in test code

--- dispatcher.lua
+++ dispatcher.lua
@@ -9,7 +9,7 @@ local api = hotload "router"()
 
 api:get("/chunked%-message", "controller/chunked_messages.lua")
 api:get("/refresh", function(request, response)
-    response:refresh("/", 5, "view/error.lua", "text/html", request.path, request.method)
+    return response:refresh("/", 5, "view/error.lua", "text/html", request.path, request.method)
 end)
 
 api:get("/auth", "controller/auth.lua")

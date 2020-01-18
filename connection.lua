4a6fc6294fea48e15c28970fe44362f93c81b6ed
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-01-18 17:32:44 +0100

    this is a new plugin that handles bound clients that idle around in the server object and wait to be processed.  along the way this plugin exposes even more xors hooks that will trigger listening plugin callbacks

---
+++ connection.lua
@@ -0,0 +1,40 @@
+local Request = hotload "request"
+local Response = hotload "response"
+local Contact = {}
+
+
+function Contact:onConnect(server, client)
+    server:hook("beforeRequest", server, client)
+    client.request = Request(client.socket)
+    if client.request.headers_received then
+        server:hook("beforeResponse", server, client)
+        client.response = Response(client.socket, client.request)
+        return -- ok
+    end
+    client.request = nil
+    -- NOTE could not parse HTTP header of client request
+    -- seem the client request can not be handled by this plugin
+    -- leave it alone, another plugin may handle the request instead...
+end
+
+
+function Contact:onProcess(server, client)
+    if client.request and client.response then
+        if client.request.header_received
+        and (not client.request.message_received
+        or not client.response.header_sent
+        or not client.response.message_sent)
+        then
+            -- emit xors hook event to trigger listening plugin callbacks
+            server:hook("onDispatch", server, client)
+        end
+        if client.request_received then
+            server:hook("afterRequest", server, client)
+        end
+        if client.response_sent then
+            server:hook("afterResponse", server, client)
+        end
+    end
+end
+
+return Contact

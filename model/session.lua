de4ffa62f5c79b80ba04622f32de988d30f33a53
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-01-21 18:52:03 +0100

    fixed bug where cookie_name was set too late

--- model/session.lua
+++ model/session.lua
@@ -12,6 +12,7 @@ Session.set_uuid = Storage.set_namespace
 
 
 function Session:new(request, response, cookie, lifetime)
+    self.cookie_name = cookie or "xors-session-id"
     local session_uuid = hash(32)
     for key, value in request.header:get("cookie", string.gmatch, "([^=; ]+)=([^=;]+)") or function() end do
         if key == self.cookie_name then
@@ -21,7 +22,6 @@ function Session:new(request, response, cookie, lifetime)
     end
     self.db = Database "db/client_session.db"
     self.uuid = session_uuid
-    self.cookie_name = cookie or "xors-session-id"
     self.cookie_lifetime = lifetime or 604800 -- 7 days (in seconds)
     response.header:set("set-cookie", self.cookie_name.."="..self.uuid.."; Max-Age="..self.cookie_lifetime) -- update or create new
 end

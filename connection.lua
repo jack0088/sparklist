5811e4a0d5960ebbcb4fa8e6c4f376f0ce69e6a4
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-01-24 18:12:07 +0100

    default values for sessions now come from database (if any) so an admin can set these props later through admin panel

--- connection.lua
+++ connection.lua
@@ -11,6 +11,7 @@ local Request = hotload "request"
 local Response = hotload "response"
 local Session = hotload "session"
 local session_gc = hotload "garbagecollect"("session")
+local xors_settings = hotload "local_storage"("xors")
 local Contact = {}
 
 
@@ -19,12 +20,23 @@ function Contact:onConnect(server, client)
     server:hook("beforeResponse", server, client)
     client.request = Request(client.socket)
     client.response = Response(client.socket, client.request)
-    client.request.header.session = Session(client, "sparklist_session")
+
+    local cookie_name = xors_settings:get("client_cookie_name")
+    local cookie_lifetime = xors_settings:get("client_cookie_lifetime")
+    if not cookie_name then
+        cookie_name = "xors_session_id"
+        xors_settings:set("client_cookie_name", cookie_name)
+    end
+    if not cookie_lifetime then
+        cookie_lifetime = 604800 -- 7 days (in seconds)
+        xors_settings:set("client_cookie_lifetime", cookie_lifetime)
+    end
+    client.request.header.session = Session(client, cookie_name, cookie_lifetime)
     
     local session_database = client.request.header.session.db.file
     local session_table = client.request.header.session.table
     local current_time = dt.timestamp()
-    local cookie_expiry = current_time + client.request.header.session.cookie_lifetime
+    local cookie_expiry = current_time + cookie_lifetime
     if cookie_expiry > current_time then
         server:insertPlugin(session_gc) -- if not yet done
         -- server.plugins[getn(server.plugins) + 1] = session_gc

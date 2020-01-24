0e83c57a6fe817fdc04ddeaf13100e25939ffd5c
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-01-24 18:09:31 +0100

    sessions are now a real subclass of local_storage

--- session.lua
+++ session.lua
@@ -5,7 +5,6 @@ local hotload = require "hotload"
 local dt = hotload "datetime"
 local hash = hotload "randomseed"
 local class = hotload "class"
-local Database = hotload "database"
 local LocalStorage = hotload "local_storage"
 local Session = class(LocalStorage)
 Session.get_table = LocalStorage.get_table
@@ -13,33 +12,23 @@ Session.set_table = LocalStorage.set_table
 
 
 function Session:new(client, cookie, lifetime)
-    assert(client.request and client.response, "client request/response object missing")
+    assert(client.request and client.response, "missing client request/response object")
+    assert(cookie, "missing set-cookie name")
+    assert(lifetime, "missing set-cookie max-age")
     
-    self.cookie_name = cookie or "xors_session_id"
     local session_uuid = hash(32)
+    local death_date = dt.date(dt.timestamp() + lifetime)
+
     for key, value in client.request.header:get("cookie", string.gmatch, "([^=; ]+)=([^=;]+)") or function() end do
-        if key == self.cookie_name then
+        if key == cookie then
             session_uuid = value
             break
         end
     end
 
-    self.db = Database "db/client_session.db"
-    self.verbose = false
-    self.table = session_uuid
-    self.cookie_lifetime = lifetime or 604800 -- 7 days (in seconds)
-    local death_date = dt.date(dt.timestamp() + self.cookie_lifetime)
-    client.response.header:set("set-cookie", self.cookie_name.."="..self.table.."; Expires="..death_date) -- update or create new
-end
-
-
-function Session:get_verbose()
-    return self.db.verbose
-end
-
-
-function Session:set_verbose(flag)
-    self.db.verbose = flag
+    LocalStorage.new(self, session_uuid, "db/client_session.db")
+    
+    client.response.header:set("set-cookie", cookie.."="..self.table.."; Expires="..death_date) -- update or create new
 end
 
 

497c12212a20f54fbdc6375dfdb9c35c7f6fba1f
herrsch_de <jack0088@me.com>
2020-02-11 12:21:24 +0100

    permission keys for users are implemented now

---
+++ permission.lua
@@ -0,0 +1,22 @@
+-- 2020 (c) kontakt@herrsch.de
+
+
+local hotload = require "hotload"
+local class = hotload "class"
+local Storage = hotload "kvstorage"
+local Permission = class(Storage)
+
+
+function Permission:new(name, description)
+    Storage.new(self, "permissions", "db/acl.db")
+    self.column1 = "name"
+    self.column2 = "description"
+    self:set(name, permission)
+end
+
+
+function Permission:exists(name)
+    return not not Storage.exists(self, name)
+end
+
+return Permission

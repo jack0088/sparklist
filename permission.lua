285625e48d78d15e05a8f97047191f34653d668c
herrsch_de <jack0088@me.com>
2020-02-12 09:55:16 +0100

    check if permission exists by name OR its id! now we can check by id as well

--- permission.lua
+++ permission.lua
@@ -1,6 +1,7 @@
 -- 2020 (c) kontakt@herrsch.de
 
 
+local getn = table.getn or function(t) return #t end -- Lua > 5.1 idom
 local hotload = require "hotload"
 local class = hotload "class"
 local Storage = hotload "kvstorage"
@@ -16,6 +17,13 @@ end
 
 
 function Permission:exists(name)
+    if type(tonumber(name)) == "number" then -- id!
+        local records = self.db:run(
+            "select id from '%s' where id = %s",
+            self.table, name
+        )
+        return getn(records) > 0 and record[1].id == tonumber(name) or false
+    end
     return not not Storage.exists(self, name)
 end
 

dd323c81c3088953df13b00ce40070faae485197
herrsch_de <jack0088@me.com>
2020-02-27 12:04:21 +0100

    embedded its own writefile function to make this module standalone

--- server.lua
+++ server.lua
@@ -1,12 +1,12 @@
 -- 2019 (c) kontakt@herrsch.de
 
+require "xors.logger"
+
 local search = require "xors.searchpath"
 search("./xors/?.lua")
 search("./xors/?/init.lua")
 
 local hotload = require "hotload"
-hotload "logger"
-
 local server = hotload "xors"{
     port = 80,
     timeout = 0.1,
--- xors/logger.lua
+++ xors/logger.lua
@@ -1,19 +1,22 @@
 -- 2019 (c) kontakt@herrsch.de
 
--- NOTE every day a new log file is created and filled with messages from the command line console
 
-local hotload = require "hotload"
-local util = hotload "utilities"
-
-local path = "log/"
 local _print = print
+local path = "log/"
+
+local function writefile(filepath, message)
+    local f = io.open(filepath, "a")
+    if not f then return end
+    f:write(message)
+    f:close()
+end
 
 function print(...)
     local dump = os.date("%H:%M:%S")
     for _, argument in ipairs{...} do
         dump = dump..tostring(argument):gsub("([^\r\n]+)", "    %1")
     end
-    util.writefile(path..os.date("%Y-%m-%d")..".txt", dump.."\n", "a")
+    writefile(path..os.date("%Y-%m-%d")..".txt", dump.."\n", "a")
     _print(dump)
 end
 

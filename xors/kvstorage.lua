64785f3331ca77c2ca86fe023966be9a34d16f16
Alexander Schäfer <herrsch@herrsch-macbook.home>
2020-03-01 01:03:39 +0100

    large rewrite; most important is that storage is now a sub-class of database; constructor can pass column names (key, value renaming)

--- xors/kvstorage.lua
+++ xors/kvstorage.lua
@@ -5,44 +5,49 @@ local getn = table.getn or function(t) return #t end -- Lua > 5.1 idom
 local hotload = require "hotload"
 local class = hotload "class"
 local Database = hotload "database"
-local Storage = class()
+local Storage = class(Database)
 
 
-function Storage:new(table_name, database_file)
-    self.db = Database(database_file or "db/xors.db")
-    self.verbose = false
-    self.table = table_name
-    self.column1 = "key" -- column name for key
-    self.column2 = "value" -- column name for value
+function Storage:new(table, column1, column2, database)
+    Database.new(self, database or "db/xors.db")
+    self.table = table
+    self.column1 = column1
+    self.column2 = column2
 end
 
 
-function Storage:get_verbose()
-    return self.db.verbose
+function Storage:create(table)
+    if type(table) == "string" and #table > 0 then
+        self:run(
+            [[create table if not exists '%s' (
+                id integer primary key autoincrement,
+                %s text unique not null,
+                %s text not null
+            )]],
+            table,
+            "key",
+            "value"
+        )
+    end
 end
 
 
-function Storage:set_verbose(flag)
-    self.db.verbose = flag
-end
-
-
-function Storage:get_table() -- getter for Storage.table
+function Storage:get_table()
     return self.__tablename
 end
 
 
-function Storage:set_table(name) -- setter for Storage.table
+function Storage:set_table(name)
     assert(type(name) == "string", "missing common identifier string")
     assert(not name:find("[^%a%d%-_]+"), "common identifier string '"..name.."' must only contain [a-zA-Z0-9%-_] characters")
     if self.table ~= nil and self.table ~= name then
-        if self.db:count(self.table) < 1 then
+        if self:count(self.table) < 1 then
             -- before switching to new db table delete the current one if it remains empty
             self:destroy()
         end
     end
     self.__tablename = name
-    self:create()
+    self:create(self.table)
 end
 
 
@@ -52,10 +57,12 @@ end
 
 
 function Storage:set_column1(name)
-    if self.__columnname1 ~= nil and self.__columnname1 ~= name then
-        self.db:rename(self.table, self.__columnname1, name)
+    if type(name) == "string" and #name > 0 then
+        if self.__columnname1 ~= nil and self.__columnname1 ~= name then
+            self:rename(self.table, self.__columnname1, name)
+        end
+        self.__columnname1 = name
     end
-    self.__columnname1 = name
 end
 
 
@@ -65,79 +72,36 @@ end
 
 
 function Storage:set_column2(name)
-    if self.__columnname2 ~= nil and self.__columnname2 ~= name then
-        self.db:rename(self.table, self.__columnname2, name)
+    if type(name) == "string" and #name > 0 then
+        if self.__columnname2 ~= nil and self.__columnname2 ~= name then
+            self:rename(self.table, self.__columnname2, name)
+        end
+        self.__columnname2 = name
     end
-    self.__columnname2 = name
-end
-
-
-function Storage:create()
-    if type(self.table) == "string" then
-        self.db:run(
-            [[create table if not exists '%s' (
-                id integer primary key autoincrement,
-                %s text unique not null,
-                %s text not null
-            )]],
-            self.table,
-            self.column1,
-            self.column2
-        )
-    end
-end
-
-
-function Storage:destroy()
-    self.db:destroy(self.table)
 end
 
 
 function Storage:exists(key, value)
     if key and value then
-        local records = self.db:run(
+        local records = self:run(
             "select id from '%s' where %s = '%s' and %s = '%s'",
             self.table, self.column1, tostring(key), self.column2, tostring(value)
         )
         return getn(records) > 0 and record[1].id or false
     elseif value then
-        local records = self.db:run(
+        local records = self:run(
             "select %s from '%s' where %s = '%s'",
             self.column1, self.table, self.column2, tostring(value)
         )
         return getn(records) > 0 and records[1][self.column1] or false
     elseif key then
-        local records = self.db:run(
+        local records = self:run(
             "select %s from '%s' where %s = '%s'",
             self.column2, self.table, self.column1, tostring(key)
         )
         return getn(records) > 0 and records[1][self.column2] or false
     end
-    return self.db:count(self.table) > 0
-end
-
-
-function Storage:set(key, value)
-    if type(key) ~= nil then
-        if type(value) ~= nil then -- upsert (update + insert)
-            if self:exists(key) then
-                self.db:run(
-                    "update '%s' set %s = '%s' where %s = '%s'",
-                    self.table, self.column2, tostring(value), self.column1, tostring(key)
-                )
-            else
-                self.db:run(
-                    "insert into '%s' (%s, %s) values ('%s', '%s')",
-                    self.table, self.column1, self.column2, tostring(key), tostring(value)
-                )
-            end
-        elseif self:exists(key) then
-            self.db:run(
-                "delete from '%s' where %s = '%s'",
-                self.table, self.column1, tostring(key)
-            )
-        end
-    end
+    return self:count(self.table) > 0
 end
 
 
@@ -146,18 +110,70 @@ function Storage:get(key)
         local value = self:exists(key)
         return value == false and nil or value
     end
-    local records = self.db:run(
+    local records = self:run(
         "select %s, %s from '%s'",
         self.column1, self.column2, self.table
     )
     if getn(records) > 0 then -- unpack rows
         local entries = {}
-        for id, row in ipairs(records) do
+        for _, row in ipairs(records) do
             local k = row[self.column1]
             local v = row[self.column2]
             entries[k] = v
         end
         return entries
+    end
+end
+
+
+function Storage:getUUID(key, value)
+    local records
+    if key and value then
+        records = self:run(
+            "select id from '%s' where %s = '%s' and %s = '%s'",
+            self.table, self.column1, tostring(key), self.column2, tostring(value)
+        )
+    elseif value then
+        records = self:run(
+            "select id from '%s' %s",
+            self.table, self.column2, tostring(value)
+        )
+    elseif key then
+        records = self:run(
+            "select id from '%s' %s",
+            self.table, self.column1, tostring(key)
+        )
+    end
+    if type(records) == "table" and getn(records) > 0 then
+        local entries = {}
+        for _, row in ipairs(records) do
+            table.insert(entries, tonumber(row.id))
+        end
+        return getn(entries) == 1 and entries[1] or entries
+    end
+end
+
+
+function Storage:set(key, value)
+    if type(key) ~= nil then
+        if type(value) ~= nil then -- upsert (update + insert)
+            if self:exists(key) then
+                self:run(
+                    "update '%s' set %s = '%s' where %s = '%s'",
+                    self.table, self.column2, tostring(value), self.column1, tostring(key)
+                )
+            else
+                self:run(
+                    "insert into '%s' (%s, %s) values ('%s', '%s')",
+                    self.table, self.column1, self.column2, tostring(key), tostring(value)
+                )
+            end
+        elseif self:exists(key) then
+            self:run(
+                "delete from '%s' where %s = '%s'",
+                self.table, self.column1, tostring(key)
+            )
+        end
     end
 end
 

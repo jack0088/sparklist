9ed332db962565b4440d9661ad99a02227838c04
herrsch_de <jack0088@me.com>
2020-03-03 15:05:33 +0100

    fixed absolete parameter form and done some  cosmetic changes

--- endpoints.lua
+++ endpoints.lua
@@ -8,14 +8,13 @@ local hotload = require "hotload"
 local api = hotload "router"()
 
 api:get("/chunked%-message", "controller/chunked_messages.lua")
-api:get("/refresh", function(request, response)
+api:get("/refresh", function(client)
     -- return response:redirect("/")
-    return response:refresh("/", 5, "view/error.lua", "text/html", request.header.path, request.header.method)
+    return client.response:refresh("/", 5, "view/error.lua", "text/html", client.request.header.path, client.request.header.method)
 end)
 
-api:get("/login", "controller/login.lua")
-api:get("/login%?id=(.+)", "controller/login.lua")
 api:get("/upload/([%w%p]+)%.(%a%a%a+)", "controller/assets.lua")
+api:get("/login%?id=(.+)", "xors/login.lua")
 api:get("/?", "controller/index.lua")
 api:any(".*", "controller/error.lua")
 
